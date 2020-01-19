79e93d727440d0a7f89af569486cbba870357c56
Tedycole <geniagulin@gmail.com>
2020-01-19 22:29:33 +0300

    Dz-1

---
+++ Async-1.js
@@ -0,0 +1,16 @@
+var fn1 = () => {
+console.log('fn1')
+return Promise.resolve(1)
+}
+
+var fn2 = () => new Promise(resolve => {
+console.log('fn2')
+setTimeout(() => resolve(2), 1000)
+})
+
+
+async function promiseReduce(asyncFunctions, reduce, initialValue) {
+
+return await Promise.resolve().then(reduce());
+
+}
